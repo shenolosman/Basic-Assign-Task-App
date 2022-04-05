7e33ac51de559dd7036c80daed77cbe794333422
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2022-04-05 20:43:31 +0200

    Adding interfaces to DAL

---
+++ So.ToDo.DataAccessLayer/Interfaces/IWorkDAL.cs
@@ -0,0 +1,14 @@
+﻿using SO.ToDo.Entities.Concrete;
+
+namespace So.ToDo.DataAccessLayer.Interfaces
+{
+    public interface IWorkDAL
+    {
+        void SaveWork(Work work);
+        void DeleteWork(Work work);
+        void UpdateWork(Work work);
+        Work GetWork(int id);
+        Work GetWorkById(int id);
+        List<Work> GetAllWorks();
+    }
+}
