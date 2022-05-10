71b3bab9937dcc51602200ce2949161620764e13
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2022-05-10 14:28:34 +0200

    Notifications come to user when assign something

--- SO.ToDo.WebAPP/Areas/Member/Controllers/NotificationController.cs
+++ SO.ToDo.WebAPP/Areas/Member/Controllers/NotificationController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using SO.ToDo.BusinessLayer.Interfaces;
+using SO.ToDo.Entities.Concrete;
+using SO.ToDo.WebAPP.Areas.Admin.Models;
 
 namespace SO.ToDo.WebAPP.Areas.Member.Controllers
 {
@@ -7,11 +11,38 @@ namespace SO.ToDo.WebAPP.Areas.Member.Controllers
     [Area("Member")]
     public class NotificationController : Controller
     {
+        private readonly INotificationService _notificationService;
+        private readonly UserManager<AppUser> _userManager;
 
-        public IActionResult GetNotification()
+        public NotificationController(INotificationService notificationService, UserManager<AppUser> userManager)
+        {
+            _notificationService = notificationService;
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> GetNotification()
         {
             TempData["Active"] = "Notification";
-            throw new NotImplementedException();
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var notification = _notificationService.GetNotRead(user.Id);
+            var models = new List<NotificationListViewModel>();
+            foreach (var item in notification)
+            {
+                var model = new NotificationListViewModel
+                {
+                    Comment = item.Comment,
+                    Id = item.Id
+                };
+                models.Add(model);
+            }
+            return View(models);
+        }
+        [HttpPost]
+        public IActionResult GetNotification(int id)
+        {
+            var updateNotification = _notificationService.GetById(id);
+            updateNotification.State = true;
+            _notificationService.Edit(updateNotification);
+            return RedirectToAction(nameof(GetNotification));
         }
     }
 }
